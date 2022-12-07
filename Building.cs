02847b0194f959c54d5a4d94056d6be462078c39
kilkellym <michael@archsmarter.com>
2022-12-07 16:49:28 -0500

    complete

---
+++ Building.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RAB_Session_05_Skills
+{
+    public class Building
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int NumFloors { get; set; }
+        public double Area { get; set; }
+
+        public Building(string name, string address, int numFloors, double area)
+        {
+            Name = name;
+            Address = address;
+            NumFloors = numFloors;
+            Area = area;
+        }
+
+        public double GetBuildingArea()
+        {
+            return Area * NumFloors;
+        }
+    }
+}
--- Command.cs
+++ Command.cs
@@ -3,10 +3,12 @@ using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
+using Autodesk.Revit.DB.Structure;
 using Autodesk.Revit.UI.Selection;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 
 #endregion
 
@@ -25,34 +27,109 @@ namespace RAB_Session_05_Skills
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            Building myBuilding = new Building("Office Building", "10 Main Street", 10, 10000);
+            myBuilding.Name = "Office Building 2";
+            Building myBuilding1 = new Building("Hospital", "20 Main Street", 20, 20000);
+            Building myBuilding2 = new Building("Apartment Building", "30 Main Street", 30, 40000);
+            Building myBuilding3 = new Building("Office Building", "40 Main Street", 5, 4000);
 
-            Selection sel = uidoc.Selection;
+            List<Building> buildingList = new List<Building>();
+            buildingList.Add(myBuilding);
+            buildingList.Add(myBuilding1);
+            buildingList.Add(myBuilding2);
+            buildingList.Add(myBuilding3);
+            buildingList.Add(new Building("Store", "50 Main Street", 2, 20000));
 
-            // Retrieve elements from database
+            Neighborhood neighborhood = new Neighborhood("Downtown", "Boston", "MA", buildingList);
 
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
+            TaskDialog.Show("Test", "There are " + neighborhood.GetBuildingCount().ToString() 
+                + " buildings in " + neighborhood.Name + " " + neighborhood.City);
 
-            // Filtered element collector is iterable
+            Utils.MyStaticMethod();
 
-            foreach (Element e in col)
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            collector.OfCategory(BuiltInCategory.OST_Rooms);
+
+            using(Transaction t = new Transaction(doc))
             {
-                Debug.Print(e.Name);
-            }
+                t.Start("Insert family");
+                foreach (SpatialElement room in collector)
+                {
+                    SpatialElementBoundaryOptions options = new SpatialElementBoundaryOptions();
+                    //IList<BoundarySegment> roomBounary = room.GetBoundarySegments(options);
 
-            // Modify document within a transaction
+                    Location loc = room.Location;
+                    LocationPoint locPoint = loc as LocationPoint;
+                    XYZ roomPoint = locPoint.Point;
 
-            using (Transaction tx = new Transaction(doc))
-            {
-                tx.Start("Transaction Name");
-                tx.Commit();
+                    FamilySymbol myFS = Utils.GetFamilySymbolByName(doc, "Desk", "60\" x 30\"");
+                    FamilyInstance myInstance = doc.Create.NewFamilyInstance(roomPoint, myFS, StructuralType.NonStructural);
+
+                    SetParameterValue(room, "Ceiling Finish", "ACT");
+                    string roomName = GetParameterValueAsString(room, "Name");
+
+                    string testString = " this is my string ";
+                    string formattedString = testString.Trim();
+                }
+                t.Commit();
             }
 
             return Result.Succeeded;
         }
+
+        private void SetParameterValue(Element currentElement, string paramName, string paramValue)
+        {
+            IList<Parameter> paramList = currentElement.GetParameters(paramName);
+            
+            //Parameter curParam = currentElement.get_Parameter(BuiltInParameter.ROOM_AREA);
+
+            //if(curParam != null)
+                //curParam.Set()
+
+            //paramList.First().Set(paramValue);
+            
+            foreach(Parameter param in paramList)
+            {
+                param.Set(paramValue);
+            }
+
+        }
+
+        private string GetParameterValueAsString(Element currentElement, string paramName)
+        {
+            string returnValue = "";
+
+            IList<Parameter> paramList = currentElement.GetParameters(paramName);
+
+            Parameter myParameter = paramList.First();
+
+            returnValue = myParameter.AsString();
+
+            return returnValue;
+        }
     }
+
+    public class Neighborhood
+    {
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public List<Building> BuildingList { get; set; }
+
+        public Neighborhood(string _name, string _city, string _state, List<Building> buildingList)
+        {
+            Name = _name;
+            City = _city;
+            State = _state;
+            BuildingList = buildingList;
+        }
+
+        public int GetBuildingCount()
+        {
+            return BuildingList.Count;
+        }
+
+
+    }
+    
 }
